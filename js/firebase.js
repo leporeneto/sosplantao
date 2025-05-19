4b340a691d4d1bacb1160b7feffce48509b5bbff
leporeneto <neto.lepore@gmail.com>
2025-05-19 08:05:21 -0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAfGykxsSTGYo6SKGhhUT1r4MMPhHIl9a0",
+  authDomain: "receitas-medicas-64a41.firebaseapp.com",
+  projectId: "receitas-medicas-64a41"
+};
+
+firebase.initializeApp(firebaseConfig);
